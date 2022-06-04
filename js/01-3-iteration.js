6c8a43b9ee287ae42957754398779774ba6562ab
OspanovaVita <vitaospanova@ukr.net>
2022-06-04 20:43:56 +0300

    Добавляет комментарии для темы "перебор объекта"

--- js/01-3-iteration.js
+++ js/01-3-iteration.js
@@ -1,5 +1,8 @@
 /*
- * Перебор через for...in и Object.keys|values|entries
+ * Перебор через for...in и 
+Object.keys - возвращает массив ключей
+Object.values - возвращает массив значений
+Object.entries
  */
 
 const feedback = {
@@ -8,32 +11,32 @@ const feedback = {
   bad: 3,
 };
 
-let totalFeedback = 0;
+let totalFeedback = 0; /*создадим переменну для хранения общего количества фитбеков*/
 
-// const keys = Object.keys(feedback);
+// const keys = Object.keys(feedback); /*это конструктор Object (получи массив ключей из обьекта feedback)*/
 
 // console.log(keys);
 
-// for (const key of keys) {
-//   console.log(key);
-//   console.log(feedback[key]);
+// for (const key of keys) { /*перебираем массив с ключами*/
+//   console.log(key); /*получаем название ключа*/
+//   console.log(feedback[key]); /*получаем значение ключа*/
 
-//   totalFeedback += feedback[key];
+//   totalFeedback += feedback[key]; /*общее количество фитбеков*/
 // }
 
 // console.log('totalFeedback: ', totalFeedback);
 
-// const values = Object.values(feedback);
+// const values = Object.values(feedback); /*возвращает массив значений ключей*/
 
 // console.log(values);
 
-// for (const value of values) {
+// for (const value of values) { /*перебираем массив с значениями ключей*/
 //   console.log(value);
 
-//   totalFeedback += value;
+//   totalFeedback += value; /*сумма фитбеков*/
 // }
 
-// console.log('totalFeedback: ', totalFeedback);
+// console.log('totalFeedback: ', totalFeedback); 
 
 
 /*автопроверка*/
